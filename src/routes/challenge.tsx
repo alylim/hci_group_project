d657b6298cb0efd55bbc3d758d3c256be7962abd
Your Name <chanhaosheng@gmail.com>
2025-11-26 22:36:44 +0800

    update leaderboard tab

--- src/routes/challenge.tsx
+++ src/routes/challenge.tsx
@@ -1,9 +1,24 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { Window } from '@/components/window'
+import { Leaderboard } from '@/components/leaderboard'
 
 export const Route = createFileRoute('/challenge')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/challenge"!</div>
+  return (
+    <div className="w-screen">
+      <div className="py-20 px-40">
+        <Window>
+          <div className="space-y-6">
+            <div className="text-center space-y-2">
+            </div>
+
+            <Leaderboard />
+          </div>
+        </Window>
+      </div>
+    </div>
+  )
 }
